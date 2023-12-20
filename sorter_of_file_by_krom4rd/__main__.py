fdd9cc2f5599d62293962dbc871acc6a3c48d446
Krom4rd <143293410+Krom4rd@users.noreply.github.com>
2023-12-20 15:54:53 +0100

    Add files via upload

---
+++ sorter_of_file_by_krom4rd/__main__.py
@@ -0,0 +1,4 @@
+import sorter_of_file_by_krom4rd
+
+
+sorter_of_file_by_krom4rd.sorter_of_files(r'C:\Users\Administrator\Desktop\adsf')
---
+++ sorter_of_file_by_krom4rd/sorter_of_file_by_krom4rd/__init__.py
@@ -0,0 +1,3 @@
+from sorter_of_file_by_krom4rd.sort import sorter_of_files
+
+__all__ = ['sorter_of_file']
