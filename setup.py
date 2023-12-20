f4e68ce8a939a5ab99dd14d26475835f26a78d24
Krom4rd <143293410+Krom4rd@users.noreply.github.com>
2023-12-20 15:57:40 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sorter_of_file_by_krom4rd',
+      version='0.1',
+      description='https://github.com/Krom4rd/Krom4rd',
+      author='Oleh Novosad',
+      author_email='krom4rd@gmail.com',
+      license='MIT',
+      packages=['sort'],
+      install_requires=['markdown',],
+      zip_safe=False)
