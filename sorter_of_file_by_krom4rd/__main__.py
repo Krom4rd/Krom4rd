d7c051c08071f3e4e20bfef68be87076ae00a9d3
Krom4rd <143293410+Krom4rd@users.noreply.github.com>
2023-12-20 15:57:08 +0100

    Delete sorter_of_file_by_krom4rd directory

--- sorter_of_file_by_krom4rd/__main__.py
+++
@@ -1,4 +0,0 @@
-import sorter_of_file_by_krom4rd
-
-
-sorter_of_file_by_krom4rd.sorter_of_files(r'C:\Users\Administrator\Desktop\adsf')
--- sorter_of_file_by_krom4rd/sorter_of_file_by_krom4rd/__init__.py
+++
@@ -1,3 +0,0 @@
-from sorter_of_file_by_krom4rd.sort import sorter_of_files
-
-__all__ = ['sorter_of_file']
