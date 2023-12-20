e1a042d5b82b7158616f977e2af054338c04ae91
Krom4rd <krom4rd@gmail.com>
2023-12-20 17:54:59 +0100

    V 0.2
    Перейменовано вкладену папку на clean-folder та функції звязані з файлом sort.py. Можливість встановлення як бібліотеки. Після встановленн можливість виклику програми через термінал: clean-folder 'path' , або імпорту і використання основної функції програми в файлах .py:
    Import clean-folder
    clean-folder.sorter_of_files('path').
    Оброблено помилку при розпакуванні архівів(яка виникала при розпакуванні архіва з паролем): оминає такі архіви. Оброблено функцію перейменування файлів яких назви дублюються (максимальна кількість однакових імен 1000000): "'name'__remame__0000001.'file-format'"
    Функція сортування розархівованих папок буде запитувати чи бажає користувач відсортувати дані папки (при введені в консоль "Y"): буде рекурсивно викликана дана програма.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='sorter_of_file_by_krom4rd',
-      version='0.1',
+      version='0.2',
       description='https://github.com/Krom4rd/Krom4rd',
       author='Oleh Novosad',
       author_email='krom4rd@gmail.com',
